c53107326b4c676e0f1f2732a9f7b4413b433a0f
kurtmctaggart <kurtmctaggart@users.noreply.github.com>
2017-11-06 13:19:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-$bands = ['july talk', 'tragically hip', 'tripping daisy'];
+$bands = ['july talk', 'tragically hip', 'tripping daisy', 'rolling stones'];
 
 foreach($bands as $band) {
     echo $band . '<br />';
 }
 
+?>
